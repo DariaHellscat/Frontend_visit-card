ba46f5a342ef2752f486c2a12acbbfd8a41076ca
Daria <Daria>
2021-07-17 23:19:09 +0300

    branch has been modified according to the version_1, JS file also added

---
+++ JS/script.js
@@ -0,0 +1,10 @@
+$(function () {
+  /* вписываем класс родителя */
+  $(".portfolio__slider").slick({
+    /* Перечисляем свойства slick слайдера */
+    arrows: false,
+    dots: true,
+    autoplay: true,
+    fade: true,
+  });
+});
